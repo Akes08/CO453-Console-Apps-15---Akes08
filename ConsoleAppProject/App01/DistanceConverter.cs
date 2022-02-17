4fc297385cfcfb3ffb15618184cc0fee630106c0
Akes.Ali <ak5210365@gmail.com>
2022-02-17 11:38:06 +0000

    part of code to convert units

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -13,14 +13,76 @@ namespace ConsoleAppProject.App01
         private double miles;
 
         private double feet;
+
+        private double toDistance;
+
+        private double fromDistance;
+
+        private string toDistance;
+
+        private string fromDistance;
         /// <summary>
         /// This runs the program
         /// </summary>
         public void Run()
         {
-            
+            OutputHeading();
+
+            InputToUnit();
+            InputFromUnit();
+
+            Console.WriteLine("To unit is : " + toUnit);
+            Console.WriteLine("From unit is " + fromUnit);
+
+
+
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
         }
 
+        private void EnterUnit()
+        {
+            Console.WriteLine("1. Feet");
+            Console.WriteLine("2. Metres");
+            Console.WriteLine("Miles");
+
+            fromDistance = Console.ReadLine();
+        }
+
+        private void InputToUnit()
+        {
+            Console.WriteLine("Enter the ToUnit");
+            Console.WriteLine("1. Feet");
+            Console.WriteLine("2. Metres");
+            Console.WriteLine("Miles");
+
+            fromDistance = Console.ReadLine();
+        }
+
+        private void InputFromUnit()
+        {
+            Console.WriteLine("Enter the fromUnit");
+            Console.WriteLine("1. Feet");
+            Console.WriteLine("2. Metres");
+            Console.WriteLine("Miles");
+
+            fromDistance = Console.ReadLine();
+        }
+
+        private void ConvertMilestoFeet()
+        {
+            feet = miles * 5280;
+        }
+
+        private void Convert()
+        {
+            if(InputFromUnit().Contains("Feet",StringComparison.CurrentCultureIgnoreCase)
+                && toUnit.Contains("Feet"))
+            {
+                fromDistance
+            }
+        }
         /// <summary>
         /// Prompts the user to enter a value for miles
         /// </summary>
@@ -29,6 +91,11 @@ namespace ConsoleAppProject.App01
 
         }
 
+        private void InputUnit()
+        {
+            Console.Write("Please enter the distance in " + fromDistance + " > ");
+
+        }
         /// <summary>
         /// Calculates 
         /// </summary>
